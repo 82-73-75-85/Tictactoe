9ebacc0d241901e38921a68b0c1fbef449b5a7f0
Riku <hayashimoto@route-zero.com>
2019-02-23 18:50:15 +0900

    Clean Tictoctoe.java

--- Tictactoe.java
+++ Tictactoe.java
@@ -7,7 +7,7 @@ public class Tictactoe{
 
       for (int y = 0; y<size; y++){
          for(int x = 0; x<size; x++){
-           masu[x][y] = 0;
+            masu[x][y] = 0;
          }
       }
 
@@ -24,37 +24,37 @@ public class Tictactoe{
             play(putX,putY,size,masu,player);
          }
 
-          String sign[] = {" ", "O", "X"};
-          for(int y = 0; y<size; y++){
-              String p = "|";
-              for(int x = 0; x<size; x++){
-                  p += sign[masu[x][y]] + "|";
-              }
-              System.out.println(p);
-          }
-          System.out.println();
-       }
-       System.out.println("引き分けです");
-    }
+         String sign[] = {" ", "O", "X"};
+         for(int y = 0; y<size; y++){
+            String p = "|";
+            for(int x = 0; x<size; x++){
+               p += sign[masu[x][y]] + "|";
+            }
+            System.out.println(p);
+         }
+         System.out.println();
+      }
+      System.out.println("引き分けです");
+   }
 
    private static void play(int putX,int putY,int size,int masu[][],int player){
-	    Scanner sc = new Scanner(System.in);
+	     Scanner sc = new Scanner(System.in);
       while(true){
-	       System.out.println("整数の0~2を入力してください（縦列）");
-	       putX = sc.nextInt();
-	       System.out.println("整数の0~2してください（横列）");
-	       putY = sc.nextInt();
-	       if(putX<0 || putY<0 || putX>=size || putY>=size){
-	          System.out.println("0~2の整数でもう一度入力してください\n");
-	          continue;
-	       }else if(masu[putX][putY]!=0){
-	          System.out.println("その場所は置かれています\nもう一度入力してください\n");
-	          continue;
-	       }else{
-          masu[putX][putY] = player;
-          break;
+         System.out.println("整数の0~2を入力してください（縦列）");
+         putX = sc.nextInt();
+         System.out.println("整数の0~2してください（横列）");
+         putY = sc.nextInt();
+         if(putX<0 || putY<0 || putX>=size || putY>=size){
+            System.out.println("0~2の整数でもう一度入力してください\n");
+            continue;
+         }else if(masu[putX][putY]!=0){
+            System.out.println("その場所は置かれています\nもう一度入力してください\n");
+            continue;
+         }else{
+            masu[putX][putY] = player;
+            break;
          }
-	    }
+      }
 
       if((masu[0][0] == player && masu[1][0] == player && masu[2][0] == player) || (masu[0][1] == player && masu[1][1] == player && masu[2][1] == player) || (masu[0][2] == player && masu[1][2] == player && masu[2][2] == player))win(player);
       if((masu[0][0] == player && masu[0][1] == player && masu[0][2] == player) || (masu[1][0] == player && masu[1][1] == player && masu[1][2] == player) || (masu[2][0] == player && masu[2][1] == player && masu[2][2] == player))win(player);
